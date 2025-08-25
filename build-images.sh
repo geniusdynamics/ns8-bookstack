96b5279b0984818080dc0d694b94b2fe445edc10
Martin Bhuong <martin@genius.ke>
2025-08-25 04:10:46 +0300

    Update to 25.07.1

--- build-images.sh
+++ build-images.sh
@@ -14,7 +14,7 @@ images=()
 repobase="${REPOBASE:-ghcr.io/geniusdynamics}"
 # Configure the image name
 reponame="bookstack"
-BOOKSTACK_TAG="25.05.2"
+BOOKSTACK_TAG="25.07.1"
 # Create a new empty container image
 container=$(buildah from scratch)
 
